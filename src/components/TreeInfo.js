a2753b67d0864102e51b93216e261837be1179f0
Dione Wilson <dionew1@gmail.com>
2020-11-07 16:52:21 -0700

    Update TreeInfo with Spinner for loading

--- src/components/TreeInfo.js
+++ src/components/TreeInfo.js
@@ -1,7 +1,40 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import Spinner from "./Spinner";
 
-const TreeInfo = (props) => {
-  return <img src={props.result.image_url} />;
-};
+class TreeInfo extends React.Component {
+  state = { isLoading: true };
+
+  imageLoaded = () => {
+    this.setState({ isLoading: false });
+  };
+
+  renderContent() {
+    return (
+      <React.Fragment>
+        <div style={{ display: this.state.isLoading ? "block" : "none" }}>
+          <Spinner />
+        </div>
+
+        <img
+          style={{ display: this.state.isLoading ? "none" : "block" }}
+          src={this.props.result.image_url}
+          alt={this.props.result.name}
+          onLoad={this.imageLoaded}
+        />
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.result.name}</h2>
+        {this.renderContent()}
+        <p>{this.props.result.description}</p>
+      </div>
+    );
+  }
+}
 
 export default TreeInfo;
