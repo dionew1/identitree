c38a083e7089672250cccd85e9664f34271b5801
Dione Wilson <dionew1@gmail.com>
2020-11-07 18:01:22 -0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-## Identitree
-
-### Summary
-
-Input your name and discover what tree you are! Used with the lovely-asa-tree API.
--- src/api/tree.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://lovely-asa-tree.herokuapp.com/api/v1",
-});
